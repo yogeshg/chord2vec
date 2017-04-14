2f0d70e8c0996bc45ed343fe39b717b7a00c293d
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-04-13 23:59:20 -0400

    adding a profile file to set paths to repos

---
+++ profile_chord2vec.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH='/tmp/yo/repo/models/tutorials/rnn/:/tmp/yo/repo/chord2vec/:/tmp/yo/repo/models/tutorials/rnn/translate/'
+#export PYTHONPATH='/tmp/yo/repo/models/:/tmp/yo/repo/chord2vec/'
+
